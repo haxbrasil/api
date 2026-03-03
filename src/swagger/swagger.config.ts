4eb55239d5ee783d4ec3db9642c3a8a24ac8c607
gabhaxball <gabhaxball@gmail.com>
2026-03-03 14:59:02 -0300

    fix: correct import path for ApiTag in swagger configuration

--- src/swagger/swagger.config.ts
+++ src/swagger/swagger.config.ts
@@ -1,5 +1,5 @@
 import { DocumentBuilder } from '@nestjs/swagger';
-import { ApiTag } from 'src/common/swagger/api-tag.enum';
+import { ApiTag } from '../common/swagger/api-tag.enum';
 
 export const SWAGGER_PATH = 'api';
 
